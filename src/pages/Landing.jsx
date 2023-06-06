3e3069680614eac86375813a40522064a6e959bc
purpleseashell888 <purpleseashell98@outlook.com>
2023-06-06 03:00:09 +0100

    first commit

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
+
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+import { useQuery } from "@tanstack/react-query";
+
+const searchCocktailQuery = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+
+    const searchTerm = url.searchParams.get("search") || "";
+    await queryClient.ensureQueryData(searchCocktailQuery(searchTerm));
+    return { searchTerm };
+  };
+
+const Landing = () => {
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktailQuery(searchTerm));
+
+  return (
+    <>
+      <SearchForm searchForm={searchTerm} />
+      <CocktailList drinks={drinks} />
+    </>
+  );
+};
+
+export default Landing;
